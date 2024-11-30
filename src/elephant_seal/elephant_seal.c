79283be473359b311090e32e23320420e1169735
Rehan <rehankarthik12@gmail.com>
2024-11-30 15:13:20 +0100

    average weight is added

--- src/elephant_seal/elephant_seal.c
+++ src/elephant_seal/elephant_seal.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 1000  // Adjust based on expected data size
+#define MAX_SIZE 5000  // Adjust based on expected data size
 
 int main() {
     FILE *file;
